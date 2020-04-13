856b69549989cfe1e28d95122a2f45af54d41300
Geogeo357 <geogeo357@gmail.com>
2020-04-13 16:08:56 -0500

    Added Firebase
    Ready to start working on making users and database actually work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { useEffect, useState } from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import Goal from './Goal'
+import firebase from './shared/firebase'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+const Welcome = ({ user }) => (
+  <AppBar position="static">
+    <Toolbar>
+      {/*<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>*/}
+      <Typography variant="h6">
+        Welcome, {user.displayName}
+      </Typography>
+      <Typography variant="h6" align="center">
+        Work2Gather
+      </Typography>
+      <Button primary onClick={() => firebase.auth().signOut()}>
+        Log out
+      </Button>
+    </Toolbar>
+  </AppBar>
+);
+
+const SignIn = () => (
+  <StyledFirebaseAuth
+    uiConfig={uiConfig}
+    firebaseAuth={firebase.auth()}
+  />
+);
+
+const Banner = ({ user, title }) => (
+  <React.Fragment>
+    { user ? <Welcome user={ user } /> : <SignIn /> }
+  </React.Fragment>
+);
 
 const App = () =>  {
   const [goalsJSON, setGoals] = useState({});
-  const [users, setUsers] = useState({});
+  const [user, setUser] = useState({});
   var goals = Object.values(goalsJSON);
   useEffect(() => {
     const fetchGoals = async () => {
@@ -16,16 +65,20 @@ const App = () =>  {
     const fetchUsers = async () => {
       const response = await fetch('./data.json');
       const json = await response.json();
-      setUsers(json.users);
+      // setUser(json.users);
       
     };
     fetchUsers();
-
   }, []);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
+
 
   return (
     <div>
-      <h1>{ "Work2Gather" }</h1>
+      <Banner user={user} title="Work2Gather"></Banner>
       {goals.map(goal => <Goal goal={goal}/>)}
     </div>
   );
