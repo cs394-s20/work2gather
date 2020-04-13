615d1691d2afb654d298af321ece6404e2eb2cc2
Geogeo357 <geogeo357@gmail.com>
2020-04-12 20:40:22 -0500

    Clean Up
    Removed initial commits that were not used

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Goal from './Goal'
 
-const work2gather = {
-  title: "Work2Gather",
-  content: ["First commit"]
-};
-
-const TaskList = ({numTasks}) => {
-  var array = Array(numTasks);
-  for (var i = 0; i < numTasks; i++){
-    array[i] = i+ 1;
-  }
-  return (
-    <React.Fragment>
-        <div>{numTasks}</div>
-        { array.map(index =>
-           <p key={index}> Hi!  I'm task {index + 1}.</p>) }
-    </React.Fragment>
-  );
-};
-
 const App = () =>  {
   const [goalsJSON, setGoals] = useState({});
   const [users, setUsers] = useState({});
-  const [numTasks, setNumTasks] = useState(0);
   var goals = Object.values(goalsJSON);
-  // var goals = {};
-  // var goal = Object.values(goalsJSON);
-  // for(var i = 0; i < inventoryKeys.length; i++){
-  //   // jsonI[inventoryKeys[i]]['sku'] = inventoryKeys[i];
-  //   inventory[inventoryKeys[i]] = inventoryJSON[inventoryKeys[i]];
-  // } 
-
   useEffect(() => {
     const fetchGoals = async () => {
       const response = await fetch('./data.json');
@@ -52,10 +25,7 @@ const App = () =>  {
 
   return (
     <div>
-      <h1>{ work2gather.title }</h1>
-      <p>{work2gather.content}</p>
-      <button onClick={() => setNumTasks(numTasks + 1)}>Click Me to create task!</button>
-      <TaskList numTasks = {numTasks}></TaskList>
+      <h1>{ "Work2Gather" }</h1>
       {goals.map(goal => <Goal goal={goal}/>)}
     </div>
   );
